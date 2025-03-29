abff6600fdf22550a58bf317fc3739f86184ebea
Gerardo Emmanuel Tovar Barragan <gerardo.tovar@lestprototype.com>
2025-03-29 11:27:20 -0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { ConnectState, CapacitorWifiConnect } from '@falconeta/capacitor-wifi-connect'
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,24 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    setTimeout(() => {
+      
+      this.secureConnect()
+    }, 2000);
+  }
+
+  async secureConnect() {
+    let { value } = await CapacitorWifiConnect.checkPermission();
+    if (value === 'prompt') {
+      const data = await CapacitorWifiConnect.requestPermission();
+      value = data.value;
+    }
+    if (value === 'granted') {
+      const a = await CapacitorWifiConnect.secureConnect({ ssid: 'Gerardo Tovar 2.4G', saveNetwork: true, password: '0D270878289', isWep: false })
+      console.log(a);
+    } else {
+      throw new Error('permission denied');
+    }
+  }
 }
